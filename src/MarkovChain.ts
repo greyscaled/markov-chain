7edcc3697dd3d8753bb3eb1d58ec1f87a1675d57
G r e y <grey@coder.com>
2020-12-05 18:04:38 -0600

    Document codebase

--- src/MarkovChain.ts
+++ src/MarkovChain.ts
@@ -1,15 +1,35 @@
 import { Validate } from "@vapurrmaid/validate";
 import { NumberMatrix, ProbabilityMatrix } from "./ProbabilityMatrix";
 
-type StateTransitionFn = ((prevState: number, nextState: number) => NumberMatrix) | undefined;
+/**
+ * StateTransitionFn returns a new ProbabilityMatrix. For advanced use
+ * previous and next states are provided.
+ */
+type StateTransitionFn = (prevState: number, nextState: number) => NumberMatrix;
 
+/**
+ * Represents a finite, discrete-time MarkovChain.
+ */
 export class MarkovChain<T> {
   private readonly values: T[];
 
   private matrix: ProbabilityMatrix;
   private state = 0;
-  private transitionFn: StateTransitionFn;
+  private transitionFn: StateTransitionFn | undefined;
 
+  /**
+   * @param values Vector mapping each state to a value
+   * @param probabilities A probabilistic NumberMatrix
+   * @param initialState The initial state; defaults to `0`
+   * @remark MarkovChains are NxN and the size of `values` and `probabilities` must be equal
+   * @throws `RangeError` if NumberMatrix does not have any rows
+   * @throws `Error` if NumberMatrix is not square
+   * @throws `Error` if NumberMatrix is not probabilistic
+   * @throws `RangeError` if `values` does not have any values
+   * @throws `Error` if `values` and `probabilities` are not the same size
+   * @throws `RangeError` if `initialState` is not within the bounds
+   * @see NumberMatrix
+   */
   constructor(values: T[], probabilities: NumberMatrix, initialState?: number) {
     this.matrix = new ProbabilityMatrix(probabilities);
 
@@ -31,27 +51,50 @@ export class MarkovChain<T> {
     }
   }
 
+  /**
+   * The current value associated to the current state
+   */
   get current(): T {
     return this.values[this.state];
   }
 
+  /**
+   * `true` if a transition function exists; otherwise `false`
+   * @see setTransitionFn
+   */
   get hasTransitionFn(): boolean {
     return this.transitionFn !== undefined;
   }
 
+  /**
+   * `true` if in the terminal state; otherwise `false
+   * @remark A terminal state implies all transitions lead back to this state
+   */
   get isTerminal(): boolean {
     const current = this.matrix.value[this.state];
     return current[this.state] === 1;
   }
 
+  /**
+   * The size of this MarkovChain
+   */
   get length(): number {
     return this.matrix.length;
   }
 
+  /**
+   * The raw, underlying probabilistic NumberMatrix used internally for computing
+   * state transitions
+   * @remark A copy is returned
+   */
   get probabilityMatrix(): NumberMatrix {
     return Array.from(this.matrix.value);
   }
 
+  /**
+   * Transitions from the current state to the next state and returns the value
+   * associated with the next state
+   */
   next(): T {
     const prevState = this.state;
     const nextState = this.matrix.selectFrom(this.state);
@@ -69,7 +112,15 @@ export class MarkovChain<T> {
     return this.values[nextState];
   }
 
-  setTransitionFn(fn: StateTransitionFn): void {
+  /**
+   * Add a custom transition function. The transition function is applied
+   * each time `next` is called to recreate a new state transition probability
+   * matrix.
+   * @param fn A transition function
+   * @remark If `fn` is `undefined`, the current transition function is removed
+   * @see ProbabilityMatrix
+   */
+  setTransitionFn(fn: StateTransitionFn | undefined): void {
     this.transitionFn = fn;
   }
 }
--- src/ProbabilityMatrix.ts
+++ src/ProbabilityMatrix.ts
@@ -1,10 +1,41 @@
 import { Validate } from "@vapurrmaid/validate";
 
+/**
+ * NumberMatrix represents a Mathematical matrix
+ */
 export type NumberMatrix = number[][];
 
+/**
+ * Represents a probability matrix (aka transition matrix, Markov matrix or
+ * stochastic matrix). In typical mathematical representation, a probability
+ * matrix is formed as: P = [pij], which represents the probability of
+ * transitioning to the `i`th column from the `j`th column. In this
+ * implementation each row vector entry represents transitioning from the `i`th
+ * row to the `j`th row.
+ * @example
+ * ```js
+ * [
+ *   [0, 1, 0],     // row 0
+ *   [0.5, 0, 0.5], // row 1
+ *   [1, 0, 0],     // row 2
+ * ];
+ * ```
+ *
+ * In the above example, row 1 (`[0.5, 0, 0.5]`) reads:
+ *
+ * - P=0.5 to transition from row 1 to row 0
+ * - P=0 to transition from row 1 to row 1
+ * - P=0.5 to transition from row 1 to row 2
+ */
 export class ProbabilityMatrix {
   private readonly matrix: NumberMatrix;
 
+  /**
+   * @param probabilities A probabilistic NumberMatrix
+   * @throws `RangeError` if NumberMatrix does not have any rows
+   * @throws `Error` if NumberMatrix is not square
+   * @throws `Error` if NumberMatrix is not probabilistic
+   */
   constructor(probabilities: NumberMatrix) {
     Validate.n(probabilities.length).isGreaterThan(0, "Probabilities array must contain entries");
     Validate.isTrue(
@@ -24,14 +55,28 @@ export class ProbabilityMatrix {
     this.matrix = matrix;
   }
 
+  /**
+   * Size of this matrix
+   * @remark ProbabilisticMatrices are NxN
+   */
   get length(): number {
     return this.matrix.length;
   }
 
+  /**
+   * @returns The underlying raw NumberMatrix
+   * @remark A copy is returned
+   */
   get value(): NumberMatrix {
     return Array.from(this.matrix);
   }
 
+  /**
+   * Obtains a row vector
+   * @param aRow The row vector to obtain
+   * @throws `RangeError` if `aRow` is outside the bounds of this matrix
+   * @see length
+   */
   getRowVector(aRow: number): number[] {
     Validate.n(aRow).inclusiveBetween(
       0,
@@ -41,6 +86,13 @@ export class ProbabilityMatrix {
     return Array.from(this.matrix[aRow]);
   }
 
+  /**
+   * Returns an index from the provided row that's computed using that rows
+   * probabilities.
+   * @param aRow The row vector to select a value from
+   * @throws `RangeError` if `aRow` is outside the bounds of this matrix
+   * @see length
+   */
   selectFrom(aRow: number): number {
     Validate.n(aRow).inclusiveBetween(
       0,
@@ -97,8 +149,10 @@ export class ProbabilityMatrix {
   private static randNum(): number {
     // Guarantees [0, 1] inclusive
     if (Math.random() === 0) {
+      /* istanbul ignore next */
       return 1;
     }
+    /* istanbul ignore next */
     return Math.random();
   }
 }
